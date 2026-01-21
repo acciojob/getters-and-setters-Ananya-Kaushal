f6e0b39a73326f94acecdb416e9df1de2f59bb5f
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-01-21 22:59:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 //complete this code
-class Person {}
+class Person {
+	
+	constructor(name,age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+	get name()
+	{
+		return this.name;
+	}
+
+	set age()
+	{
+		this.age=age;
+	}
+
+	get age()
+	{
+		return this.age;
+	}
+	class Student{
+		// String name;
+		// int age;
+
+		study()
+		{
+			console.log(`${this.name} is studying`);
+		}
+	}
+
+	class Teacher{
+		String name;
+		int age;
+
+		teach()
+		{
+			console.log(`${name} is teaching`);
+		}
+	}
+
+
+}
 
 class Student extends Person {}
 
